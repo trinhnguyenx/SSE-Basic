660db9407ab98e56e6c616cff60b22d3c337487a
CongTrinhh <nguyencongtrinhqb@gmail.com>
2024-12-14 20:21:51 +0700

    chore: remove pw

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',        
-    password: 'Trinh1406@',        
+    password: '',        
     database: 'mydb',     
     port: 3306  
   });
